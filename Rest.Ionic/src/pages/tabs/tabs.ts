89adc2335831692dd2b9eb1c5dc81c6592f3cbcf
Thelsandro <39775561+thelsandroantunes@users.noreply.github.com>
2020-01-05 16:34:53 -0400

    navigation

--- Rest.Ionic/src/pages/tabs/tabs.ts
+++ Rest.Ionic/src/pages/tabs/tabs.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App } from 'ionic-angular';
+
 
 /**
  * Generated class for the TabsPage page.
@@ -15,11 +16,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TabsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public app: App)
+     {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TabsPage');
+  }
+
+  selecionarCategoria(event): void{
+    
+    let navAnt = event.linker._history[event.linker._history.length - 2];
+        
+    if (event.tabTitle == 'Categoria' && navAnt != '/categoria') {
+      this.app.getRootNav().setRoot('CategoriaPage');
+    }
   }
 
 }
