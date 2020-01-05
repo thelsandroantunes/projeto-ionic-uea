a0fbf799f9f3dc13b22bef0220ce0432d7d97b09
Thelsandro <39775561+thelsandroantunes@users.noreply.github.com>
2020-01-05 00:12:55 -0400

    mongoose
    conexão com mongoDb OK!

--- Rest.Api/bin/configuration/variables.js
+++ Rest.Api/bin/configuration/variables.js
@@ -3,7 +3,7 @@ const variables = {
         port: process.env.port || 3000
     },
     Database: {
-        connection: process.env.connection || 'mongodb+srv://admin:<rest#123>@cluster0-04viw.mongodb.net/test?retryWrites=true&w=majority'
+        connection: process.env.connection || 'mongodb+srv://restadmin:rest123@cluster0-mho0h.mongodb.net/test'
     }
 }
 
--- Rest.Api/bin/express.js
+++ Rest.Api/bin/express.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
 
 //routers
 const categoriaRouter = require('../routes/categoria-router');
 const produtoRouter = require('../routes/produto-router');
 
 //Criando  a API/Server Web do Express
-const app = express();
+const app = express(variables.Database.connection);
 
 //Configuração parser do JSON
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //Configurando a conexão com MongoDB
-mongoose.connect('mongodb+srv://admin:<rest#123>@cluster0-04viw.mongodb.net/test?retryWrites=true&w=majority');
+
+mongoose.connect(variables.Database.connection, { 
+    useUnifiedTopology: true ,
+    useNewUrlParser: true 
+})
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log(`MongoDB connection error: ${err}`));;
+
 
 //Configuração das rotas
 app.use('/API/categoria', categoriaRouter);
